c47228af57b4c5c01c7a60adc5a009c6407e2d35
supriya-s13 <83180991+supriya-s13@users.noreply.github.com>
2023-12-05 18:42:09 +0530

    Update cla.java

--- cla.java
+++ cla.java
@@ -1,11 +1,1 @@
-// Java Program to Illustrate First Argument
 
-class cla {
-//cla command line argument
- 
-    // Main driver method
-    public static void main(String[] args)
-    {
-        // Printing the first argument
-        System.out.println(args[0]);
-    }
